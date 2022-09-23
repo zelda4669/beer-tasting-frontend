abb82f4f617d8cddd2f17a097ffc9a638144f5d2
Noelle Ferrari <ferrari.noelle@gmail.com>
2022-09-23 12:01:36 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
-function App() {
+function Brewery({ item }) {
+  console.log('in component', item)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{item.name}</p>
+      <p>{item.location}</p>
+      <p>Tasted? {item.tasted}</p>
     </div>
-  );
+  )
 }
 
-export default App;
+function App() {
+  const breweries = [
+    {
+      'name': 'Postdoc',
+      'location': 'Redmond',
+      'tasted': true,
+      'id': 1
+    },
+    {
+      'name': 'Aslan',
+      'location': 'Bellingham',
+      'tasted': false,
+      'id': 2
+    },
+    {
+      'name': 'Nunchucks',
+      'location': 'Lynnwood',
+      'tasted': false,
+      'id': 3
+    }
+  ]
+
+  console.log('in app', breweries[0])
+
+  return (
+    <Brewery item={breweries[0]} />
+  )
+}
+
+export default App
